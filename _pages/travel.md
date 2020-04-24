3a9c06ce3f3edc4de934e4b87cf100abc05cce7a
Lindt8 <hratliff8@gmail.com>
2020-04-25 00:53:39 +0900

    added travelmap

---
+++ _pages/travel.md
@@ -0,0 +1,32 @@
+---
+layout: archive
+title: "Travel"
+permalink: /travel/
+author_profile: true
+redirect_from:
+  - /travels
+---
+
+{% include base_path %}
+
+<style>
+.tablelines table, .tablelines td, .tablelines th {
+        border: 1px solid black;
+        }
+.tablelines th, .tablelines td {
+          width: 100%;
+        }     
+.tablelines th:first-child,  .tablelines td:first-child {
+          width: 8%;
+        }
+</style>
+
+
+Below is an interactive map showing most of the places I have traveled for work and for fun!
+
+<iframe src="/travelmap/map.html" height="700" width="850" style="border:none;"></iframe>
+
+
+
+<!-- [Click to view a map of all of the talks I have given.](http://lindt8.github.io/talkmap.html) -->
+<!-- <embed src="http://lindt8.github.io/files/CV_Hunter_Ratliff.pdf" width="650" height="1800" type='application/pdf'> -->
---
+++ travelmap/talkmap/org-locations.js
@@ -0,0 +1,117 @@
+var addressPoints = [
+  [
+    "1050 Walnut Street, Boulder, CO 80302",
+    40.0163197,
+    -105.2814085
+  ],
+  [
+    "701 Briarcliff Avenue, Oak Ridge, TN 37830",
+    36.022695,
+    -84.2317223
+  ],
+  [
+    "828 Paseo De Peralta, Santa Fe, NM 87501",
+    35.686505,
+    -105.9347528
+  ],
+  [
+    "Gofuku, Toyama-shi, Toyama-ken, Japan",
+    36.7004692,
+    137.1858656
+  ],
+  [
+    "San Antonio, TX, USA",
+    29.4246002,
+    -98.4951405
+  ],
+  [
+    "Orlando, FL, USA",
+    28.5421109,
+    -81.3790304
+  ],
+  [
+    "Washington D.C., USA",
+    38.8949855,
+    -77.0365708
+  ],
+  [
+    "98 Rochester St, Upton, NY 11973, United States",
+    40.8793984,
+    -72.8716963
+  ],
+  [
+    "New York City, NY, USA",
+    40.7127281,
+    -74.0060152
+  ],
+  [
+    "1 Nasa Dr, Hampton, VA 23666, United States",
+    37.0829365,
+    -76.3800837
+  ],
+  [
+    "Hitachi-shi, Ibaraki-ken, Japan",
+    36.5890835,
+    140.7639580530664
+  ],
+  [
+    "Tokyo, Japan",
+    35.6828387,
+    139.7594549
+  ],
+  [
+    "Kawagoe, Saitama-ken, Japan",
+    35.9251145,
+    139.4856927
+  ],
+  [
+    "Hokkaido, Japan",
+    43.4603175,
+    143.33071579283663
+  ],
+  [
+    "Tateyama, Nakaniikawa, Toyama-ken, Japan",
+    36.6634944,
+    137.3139395
+  ],
+  [
+    "CEA, Saclay, France",
+    48.72456055,
+    2.148065389696782
+  ],
+  [
+    "Boeretang 200, 2400 Mol, Belgium",
+    51.21904055,
+    5.093510909317855
+  ],
+  [
+    "Ghent, Belgium",
+    51.0538286,
+    3.7250121
+  ],
+  [
+    "Bruges, Belgium",
+    51.2085526,
+    3.226772
+  ],
+  [
+    "Aizuwakamatsu, Fukushima-ken, Japan",
+    37.48043,
+    139.942135
+  ],
+  [
+    "Fujikawaguchiko, Minamitsuru, Yamanashi-ken, Japan",
+    35.5010653,
+    138.7653041
+  ],
+  [
+    "Mito-shi, Ibaraki-ken, Japan",
+    36.3657792,
+    140.4713933
+  ],
+  [
+    "Paris, France",
+    48.8566969,
+    2.3514616
+  ]
+];
