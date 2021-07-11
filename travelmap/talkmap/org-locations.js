91ce4a01344f1e4b69fcbb069e15dea64d9e9347
Lindt8 <Lindt8@users.noreply.github.com>
2021-07-11 18:59:24 +0900

    updated travel map

--- travelmap/talkmap/org-locations.js
+++ travelmap/talkmap/org-locations.js
@@ -1,5 +1,10 @@
 var addressPoints = [
   [
+    "Hamarikyu Gardens, Tokyo, Japan",
+    35.660176,
+    139.76381456673238
+  ],
+  [
     "San Antonio, TX, USA",
     29.4246002,
     -98.4951405
@@ -13,6 +18,16 @@ var addressPoints = [
     "Atlanta, GA, USA",
     33.7489924,
     -84.3902644
+  ],
+  [
+    "Tokyo Tower, Tokyo, Japan",
+    35.65858645,
+    139.74544005796224
+  ],
+  [
+    "\u3012648-0211 \u548c\u6b4c\u5c71\u770c\u4f0a\u90fd\u90e1\u9ad8\u91ce\u753a",
+    34.215788,
+    135.5872944
   ],
   [
     "Washington D.C., USA",
@@ -56,8 +71,8 @@ var addressPoints = [
   ],
   [
     "828 Paseo De Peralta, Santa Fe, NM 87501",
-    35.686505,
-    -105.9347528
+    35.68642535,
+    -105.9348916994722
   ],
   [
     "Hitachi-shi, Ibaraki-ken, Japan",
@@ -131,8 +146,8 @@ var addressPoints = [
   ],
   [
     "CEA, Saclay, France",
-    48.7290281,
-    2.1457424
+    48.7094984,
+    2.148721749110957
   ],
   [
     "Paris, France",
@@ -218,5 +233,195 @@ var addressPoints = [
     "Tokai-mura, Naka-gun, Ibaraki-ken, Japan",
     36.4726607,
     140.5662106
+  ],
+  [
+    "Kinkakuji, Kyoto, Japan",
+    35.03952925,
+    135.72953725309281
+  ],
+  [
+    "Ryoanji, Kyoto, Japan",
+    35.0279007,
+    135.7211489
+  ],
+  [
+    "Tenryuji, Kyoto, Japan",
+    35.0159406,
+    135.6765504
+  ],
+  [
+    "Jojakkoji, Kyoto, Japan",
+    35.019691699999996,
+    135.66864633784195
+  ],
+  [
+    "Saga Toriimoto Preserved Street, Kyoto, Japan",
+    35.0264196,
+    135.6668057
+  ],
+  [
+    "Pontocho Alley, Kyoto, Japan",
+    35.0080674,
+    135.7710399
+  ],
+  [
+    "Ginkakuji, Kyoto, Japan",
+    35.0268996,
+    135.7983713692728
+  ],
+  [
+    "Nanzenji, Kyoto, Japan",
+    35.011287,
+    135.79024
+  ],
+  [
+    "Heian Shrine, Kyoto, Japan",
+    35.015906799999996,
+    135.78289780423577
+  ],
+  [
+    "Yasaka Shrine, Kyoto, Japan",
+    35.00360265,
+    135.77826053837538
+  ],
+  [
+    "Kodaiji, Kyoto, Japan",
+    35.00174645,
+    135.77985591939222
+  ],
+  [
+    "Kiyomizudera, Kyoto, Japan",
+    34.994303,
+    135.7844388864188
+  ],
+  [
+    "Sanmon, Kyoto, Japan",
+    35.1147593,
+    135.7727746
+  ],
+  [
+    "Fushimi Inari Taisha, Kyoto, Japan",
+    34.967519249999995,
+    135.77971008109822
+  ],
+  [
+    "Byodoin, Uji, Kyoto, Japan",
+    34.8895001,
+    135.8074525242721
+  ],
+  [
+    "Nijojo, Kyoto, Japan",
+    35.0118669,
+    135.7504794
+  ],
+  [
+    "Nara National Museum, Nara, Japan",
+    34.6830955,
+    135.83831048890232
+  ],
+  [
+    "Nandaimon Gate, Nara, Japan",
+    34.6857978,
+    135.83987367510116
+  ],
+  [
+    "Todaiji, Nara, Japan",
+    34.71272695,
+    135.78870005471012
+  ],
+  [
+    "Kasugataisha, Nara, Japan",
+    34.6831052,
+    135.8399756
+  ],
+  [
+    "Kongobuji, Koyasan, Wakayama, Japan",
+    34.2131319,
+    135.5838267
+  ],
+  [
+    "Daimon Gate, Koyasan, Wakayama, Japan",
+    34.21314955,
+    135.57285804799386
+  ],
+  [
+    "Nyonindo, Koyasan, Wakayama, Japan",
+    34.2188849,
+    135.5811088
+  ],
+  [
+    "Okunoin, Koyasan, Wakayama, Japan",
+    34.2193436,
+    135.60512464045058
+  ],
+  [
+    "Sannenzaka, Kyoto, Japan",
+    34.9974947,
+    135.7810632
+  ],
+  [
+    "Konpon Daito, Koyasan, Wakayama, Japan",
+    34.213437049999996,
+    135.5799621
+  ],
+  [
+    "Giouji, Ukyo Ward, Kyoto, Japan",
+    35.023516900000004,
+    135.66680060601067
+  ],
+  [
+    "K\u014dfuku-ji, Nara, Japan",
+    34.683068399999996,
+    135.8335150936077
+  ],
+  [
+    "Osaka Castle, Osaka, Japan",
+    34.6873398,
+    135.5258386
+  ],
+  [
+    "Abeno Harukas, Osaka, Japan",
+    34.645923249999996,
+    135.51396582509852
+  ],
+  [
+    "D\u014dtonbori, Osaka, Japan",
+    34.6690306,
+    135.5015715
+  ],
+  [
+    "Osaka Aquarium Kaiyukan, Osaka, Japan",
+    34.6545464,
+    135.42892109889533
+  ],
+  [
+    "Shinsekai, Osaka, Japan",
+    34.6520901,
+    135.5061908
+  ],
+  [
+    "Odaiba, Tokyo, Japan",
+    35.619050200000004,
+    139.77936404588297
+  ],
+  [
+    "Tokyo National Museum, Tokyo, Japan",
+    35.71910615,
+    139.77601900459837
+  ],
+  [
+    "Tokyo Skytree, Tokyo, Japan",
+    35.71005425,
+    139.81071409992649
+  ],
+  [
+    "Kasuga-taisha Shrine, Nara, Japan",
+    34.6807615,
+    135.8463146
+  ],
+  [
+    "T\u014ddai-ji Temple, Nara, Japan",
+    34.6874989,
+    135.839814
   ]
 ];
