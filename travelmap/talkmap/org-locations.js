9ce04dee7f6f4409470001759bc27f8840dfd820
Lindt8 <Lindt8@users.noreply.github.com>
2024-12-28 16:10:48 +0100

    updated travel map

--- travelmap/talkmap/org-locations.js
+++ travelmap/talkmap/org-locations.js
@@ -1,8 +1,578 @@
 var addressPoints = [
   [
-    "Hamarikyu Gardens, Tokyo, Japan",
-    35.660176,
-    139.76381456673238
+    "Dresden, Germany",
+    51.0493286,
+    13.7381437
+  ],
+  [
+    "Helmholtz-Zentrum Dresden-Rossendorf, Dresden, Germany",
+    51.057451,
+    13.953310881452225
+  ],
+  [
+    "Saxon Switzerland National Park, Germany",
+    50.9148724,
+    14.286473124246285
+  ],
+  [
+    "Physikalisch-Technische Bundesanstalt, Braunschweig, Germany",
+    52.29641355,
+    10.465579540656728
+  ],
+  [
+    "Braunschweig, Germany",
+    52.2646577,
+    10.5236066
+  ],
+  [
+    "Zagreb, Croatia",
+    45.8130967,
+    15.9772795
+  ],
+  [
+    "Institut Ru\u0111er Bo\u0161kovi\u0107, Zagreb, Croatia",
+    45.82962190000001,
+    15.987950843597645
+  ],
+  [
+    "Milano, Italy",
+    45.4641943,
+    9.1896346
+  ],
+  [
+    "Lillestr\u00f8m, Norway",
+    59.91553955,
+    11.217035409234612
+  ],
+  [
+    "Voss, Norway",
+    60.6837067,
+    6.407905054203272
+  ],
+  [
+    "Rosendal, Norway",
+    59.9869792,
+    6.0067979
+  ],
+  [
+    "Barcelona, Spain",
+    41.3828939,
+    2.1774322
+  ],
+  [
+    "London, United Kingdom",
+    51.5074456,
+    -0.1277653
+  ],
+  [
+    "Geilo, Norway",
+    60.5345261,
+    8.2063719
+  ],
+  [
+    "Walt Disney World Resort, Florida, USA",
+    28.3824072,
+    -81.56170781271818
+  ],
+  [
+    "Universal Studios, Florida, USA",
+    28.4687685,
+    -81.4669503
+  ],
+  [
+    "Troms\u00f8, Norway",
+    69.6516345,
+    18.9558585
+  ],
+  [
+    "Prague, Czech Republic",
+    50.0596288,
+    14.446459273258009
+  ],
+  [
+    "Orpington, United Kingdom",
+    51.3796373,
+    0.1053732
+  ],
+  [
+    "Hastings, United Kingdom",
+    50.8553888,
+    0.5824703
+  ],
+  [
+    "Tonbridge, United Kingdom",
+    51.1955494,
+    0.2750712
+  ],
+  [
+    "Hever Castle, Kent, United Kingdom",
+    51.1869092,
+    0.11373480835010619
+  ],
+  [
+    "Svolv\u00e6r, Nordland, Norway",
+    68.2321709,
+    14.5646019
+  ],
+  [
+    "Henningsv\u00e6r, Nordland, Norway",
+    68.152888,
+    14.2007245
+  ],
+  [
+    "Ballstad, Nordland, Norway",
+    68.0742983,
+    13.5450454
+  ],
+  [
+    "Nusfjord, Nordland, Norway",
+    68.0354096,
+    13.3477048
+  ],
+  [
+    "Lofotr Viking Museum, Nordland, Norway",
+    68.244219,
+    13.757639
+  ],
+  [
+    "Rambergstranda, Nordland, Norway",
+    68.0984029,
+    13.2455649
+  ],
+  [
+    "Hamn\u00f8y, Nordland, Norway",
+    67.949777,
+    13.136904
+  ],
+  [
+    "Reine, Nordland, Norway",
+    67.9324675,
+    13.0887684
+  ],
+  [
+    "\u00c5, Nordland, Norway",
+    67.8796157,
+    12.9840442
+  ],
+  [
+    "Jack Daniel's Distillery, Lynchburg, TN, USA",
+    35.283600050000004,
+    -86.36994737938207
+  ],
+  [
+    "Nashville, TN, USA",
+    36.1622767,
+    -86.7742984
+  ],
+  [
+    "Rockefeller Center, New York City, NY, USA",
+    40.75925755,
+    -73.97995686116377
+  ],
+  [
+    "Grand Central Terminal, New York City, NY, USA",
+    40.75269435,
+    -73.97725295036929
+  ],
+  [
+    "One Vanderbilt, New York City, NY, USA",
+    40.75297175,
+    -73.97854231036098
+  ],
+  [
+    "Trump Tower, New York City, NY, USA",
+    40.7623148,
+    -73.97390282125886
+  ],
+  [
+    "Central Park, New York City, NY, USA",
+    40.7827725,
+    -73.9653627406542
+  ],
+  [
+    "Edge, Hudson Yards, New York City, NY, USA",
+    40.7539509,
+    -74.0006727
+  ],
+  [
+    "One World Trade Center, New York City, NY, USA",
+    40.71299915,
+    -74.01318943816048
+  ],
+  [
+    "Chelsea Market, New York City, NY, USA",
+    40.7420513,
+    -74.0048973
+  ],
+  [
+    "Empire State Building, New York City, NY, USA",
+    40.74844205,
+    -73.98565890160751
+  ],
+  [
+    "University of Tennessee, Knoxville, TN, USA",
+    35.9515612,
+    -83.93087998929315
+  ],
+  [
+    "Great Smoky Mountains National Park, TN, USA",
+    35.634220049999996,
+    -83.40236602525079
+  ],
+  [
+    "Anakeesta, Gatlinburg, TN, USA",
+    35.71308845,
+    -83.51180224975701
+  ],
+  [
+    "Clingmans Dome, USA",
+    35.5628637,
+    -83.498379
+  ],
+  [
+    "Cherokee, NC, USA",
+    35.1184149,
+    -84.0886181
+  ],
+  [
+    "Blue Ridge Parkway, NC, USA",
+    36.147757150000004,
+    -81.71509781328461
+  ],
+  [
+    "Cades Cove, TN, USA",
+    35.5936699,
+    -83.8249969
+  ],
+  [
+    "Laurel Falls, TN, USA",
+    36.272754,
+    -82.134928
+  ],
+  [
+    "Foothills Parkway, TN, USA",
+    35.81320320529314,
+    -83.24621659584611
+  ],
+  [
+    "Country Music Hall of Fame and Museum, Nashville, TN, USA",
+    36.1581728,
+    -86.77609291089297
+  ],
+  [
+    "Munich, Germany",
+    48.1371079,
+    11.5753822
+  ],
+  [
+    "Residenz M\u00fcnchen, Munich, Germany",
+    48.1411608,
+    11.57904463904056
+  ],
+  [
+    "Nymphenburg Palace, Munich, Germany",
+    48.1582569,
+    11.5032967
+  ],
+  [
+    "Neuschwanstein Castle, Germany",
+    47.5575522,
+    10.749695890887043
+  ],
+  [
+    "Kehlsteinhaus, Germany",
+    47.6114847,
+    13.042059677615596
+  ],
+  [
+    "Berchtesgaden, Germany",
+    47.6330221,
+    13.0020051
+  ],
+  [
+    "Salzburg, Austria",
+    47.7981346,
+    13.0464806
+  ],
+  [
+    "Hallstatt, Austria",
+    47.5621698,
+    13.6486697
+  ],
+  [
+    "Oxford, United Kingdom",
+    51.7520131,
+    -1.2578499
+  ],
+  [
+    "Blenheim Palace, Woodstock, United Kingdom",
+    51.845819,
+    -1.3507135
+  ],
+  [
+    "Cambridge, United Kingdom",
+    52.2055314,
+    0.1186637
+  ],
+  [
+    "Trolltunga, Norway",
+    60.1329576,
+    6.7539979
+  ],
+  [
+    "Odda, Norway",
+    60.0679248,
+    6.5469316
+  ],
+  [
+    "L\u00e5tefossen, Norway",
+    59.9478953,
+    6.58476
+  ],
+  [
+    "Skjervsfossen, Norway",
+    60.587935,
+    6.6364171
+  ],
+  [
+    "Dale, Vaksdal, Norway",
+    60.5899527,
+    5.8211245
+  ],
+  [
+    "Fl\u00e5m, Norway",
+    60.8629525,
+    7.1131783
+  ],
+  [
+    "Stegastein, Aurland, Norway",
+    60.9086345,
+    7.2117732
+  ],
+  [
+    "Glesv\u00e6r, Norway",
+    60.2046211,
+    5.0402788
+  ],
+  [
+    "Stavanger, Norway",
+    58.9699675,
+    5.7318101
+  ],
+  [
+    "Preikestolen, Norway",
+    58.98402089991217,
+    6.185542956014302
+  ],
+  [
+    "Sverd i fjell, Stavanger, Norway",
+    58.9413693,
+    5.6718992
+  ],
+  [
+    "Tiritiri Matangi, New Zealand",
+    -36.600131399999995,
+    174.88914118513316
+  ],
+  [
+    "Auckland, New Zealand",
+    -36.852095,
+    174.7631803
+  ],
+  [
+    "Waitomo Glowworm Caves, New Zealand",
+    -38.26086905,
+    175.10354285598802
+  ],
+  [
+    "Otorohanga Kiwi House, New Zealand",
+    -38.17994995,
+    175.21368618903756
+  ],
+  [
+    "Hamilton, New Zealand",
+    -37.7878809,
+    175.281788
+  ],
+  [
+    "Hobbiton, New Zealand",
+    -37.8727298,
+    175.68356454078355
+  ],
+  [
+    "Rotorua, New Zealand",
+    -38.1381493,
+    176.252922
+  ],
+  [
+    "Redwoods \u2013 Whakarewarewa Forest, Rotorua, New Zealand",
+    -38.19610255,
+    176.28541934372083
+  ],
+  [
+    "Wai-O-Tapu Thermal Wonderland, Rotorua, New Zealand",
+    -38.3558981,
+    176.367513
+  ],
+  [
+    "Mitai Maori Village, Rotorua, New Zealand",
+    -38.1073014,
+    176.2200731
+  ],
+  [
+    "Huka Falls, Taupo, New Zealand",
+    -38.663948914553586,
+    176.07989525978263
+  ],
+  [
+    "Taupo, New Zealand",
+    -38.6866197,
+    176.0694773
+  ],
+  [
+    "Wellington, New Zealand",
+    -41.2887953,
+    174.7772114
+  ],
+  [
+    "Te Papa Tongarewa Museum, Wellington, New Zealand",
+    -41.2903326,
+    174.78192750276241
+  ],
+  [
+    "Mount Victoria, Wellington, New Zealand",
+    -41.296076,
+    174.7943018
+  ],
+  [
+    "Zealandia, Wellington, New Zealand",
+    -41.30300695,
+    174.74250829736746
+  ],
+  [
+    "Picton, New Zealand",
+    -41.290916,
+    174.006908
+  ],
+  [
+    "Nelson, New Zealand",
+    -41.2710849,
+    173.2836756
+  ],
+  [
+    "Constant Quay, Charleston, New Zealand",
+    -41.90361097921524,
+    171.43567448647076
+  ],
+  [
+    "Pancake Rocks and Blowholes, New Zealand",
+    -42.1138741,
+    171.3269333
+  ],
+  [
+    "Greymouth, New Zealand",
+    -42.4499469,
+    171.2079875
+  ],
+  [
+    "Franz Josef, New Zealand",
+    -43.3889668,
+    170.1819048
+  ],
+  [
+    "Franz Josef Glacier Walk, New Zealand",
+    -43.42536436206857,
+    170.16752461523836
+  ],
+  [
+    "Lake Matheson Walk, New Zealand",
+    -43.439175430370724,
+    169.96228109368607
+  ],
+  [
+    "Fox Glacier, New Zealand",
+    -43.51702845,
+    170.1386965631589
+  ],
+  [
+    "Haast Pass, New Zealand",
+    -44.1072916,
+    169.3547989
+  ],
+  [
+    "Blue Pools Track, New Zealand",
+    -44.16003822471959,
+    169.2793914780128
+  ],
+  [
+    "Wanaka, New Zealand",
+    -44.6941691,
+    169.1364637
+  ],
+  [
+    "Queenstown, New Zealand",
+    -45.0321923,
+    168.661
+  ],
+  [
+    "Onsen Hot Pools, Queenstown, New Zealand",
+    -44.9809952,
+    168.6854275
+  ],
+  [
+    "Shotover Jet, Queenstown, New Zealand",
+    -44.98711265,
+    168.67135204548737
+  ],
+  [
+    "Te Anau, New Zealand",
+    -45.41449,
+    167.717489
+  ],
+  [
+    "Glow Worm Caves, Te Anau, New Zealand",
+    -45.4177191,
+    167.7115277
+  ],
+  [
+    "Milford Sound, New Zealand",
+    -44.61901895,
+    167.8687603063546
+  ],
+  [
+    "Doubtful Sound, New Zealand",
+    -45.3738793,
+    167.0865205429978
+  ],
+  [
+    "Cromwell, New Zealand",
+    -45.0371308,
+    169.1972881
+  ],
+  [
+    "Lindis Pass, New Zealand",
+    -44.5882604,
+    169.6430798
+  ],
+  [
+    "Kettle Hole Track, New Zealand",
+    -44.1839543,
+    170.1582652
+  ],
+  [
+    "Mount Cook, New Zealand",
+    -43.5950017,
+    170.1421677
+  ],
+  [
+    "Lake Tekapo, New Zealand",
+    -44.0045194,
+    170.4777273
+  ],
+  [
+    "Christchurch, New Zealand",
+    -43.530955,
+    172.6364343
   ],
   [
     "San Antonio, TX, USA",
@@ -20,29 +590,19 @@ var addressPoints = [
     -84.3902644
   ],
   [
-    "Tokyo Tower, Tokyo, Japan",
-    35.65858645,
-    139.74544005796224
-  ],
-  [
-    "\u3012648-0211 \u548c\u6b4c\u5c71\u770c\u4f0a\u90fd\u90e1\u9ad8\u91ce\u753a",
-    34.215788,
-    135.5872944
-  ],
-  [
     "Washington D.C., USA",
-    38.8949924,
-    -77.0365581
+    38.8950368,
+    -77.0365427
   ],
   [
     "1 Bethel Valley Rd, Oak Ridge, TN 37830, United States",
-    35.983302,
+    35.9833,
     -84.222967
   ],
   [
     "Martin Rd SW, Huntsville, AL 35808",
-    34.644626,
-    -86.694745
+    34.6446234102329,
+    -86.69479566935625
   ],
   [
     "500 S State St, Ann Arbor, MI 48109",
@@ -56,8 +616,8 @@ var addressPoints = [
   ],
   [
     "98 Rochester St, Upton, NY 11973, United States",
-    40.8644576,
-    -72.8799235
+    40.8643590919114,
+    -72.87995999536241
   ],
   [
     "New York City, NY, USA",
@@ -66,8 +626,8 @@ var addressPoints = [
   ],
   [
     "1 Nasa Dr, Hampton, VA 23666, United States",
-    37.0829365,
-    -76.3800837
+    37.083035963442164,
+    -76.38010667512468
   ],
   [
     "828 Paseo De Peralta, Santa Fe, NM 87501",
@@ -81,8 +641,8 @@ var addressPoints = [
   ],
   [
     "Tokyo, Japan",
-    35.6828387,
-    139.7594549
+    35.6768601,
+    139.7638947
   ],
   [
     "Kawagoe, Saitama-ken, Japan",
@@ -91,8 +651,8 @@ var addressPoints = [
   ],
   [
     "Nikko-shi, Tochigi-ken, Japan",
-    36.7196755,
-    139.6989028
+    36.7197576,
+    139.698139
   ],
   [
     "Kushiro-shi, Hokkaido, Japan",
@@ -106,8 +666,8 @@ var addressPoints = [
   ],
   [
     "Shirogane, Biei, Kamikawa, Hokkaido, Japan",
-    43.49368990000001,
-    142.61413004878725
+    43.49349015,
+    142.61409759665082
   ],
   [
     "Asahikawa-shi, Hokkaido, Japan",
@@ -146,13 +706,13 @@ var addressPoints = [
   ],
   [
     "CEA, Saclay, France",
-    48.7094984,
-    2.148721749110957
+    48.7290281,
+    2.1457424
   ],
   [
     "Paris, France",
-    48.8566969,
-    2.3514616
+    48.8588897,
+    2.3200410217200766
   ],
   [
     "Boeretang 200, 2400 Mol, Belgium",
@@ -181,8 +741,8 @@ var addressPoints = [
   ],
   [
     "Mito-shi, Ibaraki-ken, Japan",
-    36.3657792,
-    140.4713933
+    36.3659174,
+    140.4731743
   ],
   [
     "Oarai, Ibaraki-ken, Japan",
@@ -191,13 +751,13 @@ var addressPoints = [
   ],
   [
     "Nasu, Tochigi-ken, Japan",
-    37.019786,
-    140.1208822
+    37.0198382,
+    140.1208649
   ],
   [
-    "Fukuroda Falls, Daigo, Ibaraki, Japan",
-    36.7644411,
-    140.4070824
+    "Fukurodanotaki, Daigo, Ibaraki, Japan",
+    36.7585557,
+    140.381959
   ],
   [
     "Zenkoji Temple, Nagano, Japan",
@@ -211,8 +771,8 @@ var addressPoints = [
   ],
   [
     "Jigokudani, Nagano-ken, Japan",
-    36.73276,
-    138.4611802
+    36.732811749999996,
+    138.46231848034455
   ],
   [
     "Asakusa, Tokyo, Japan",
@@ -220,19 +780,14 @@ var addressPoints = [
     139.7975626
   ],
   [
-    "Tullahoma, TN, USA",
-    35.3620235,
-    -86.2094342
+    "Hamarikyu Gardens, Tokyo, Japan",
+    35.660176,
+    139.76382390703512
   ],
   [
-    "Knoxville, TN, USA",
-    35.9603948,
-    -83.9210261
-  ],
-  [
-    "Tokai-mura, Naka-gun, Ibaraki-ken, Japan",
-    36.4726607,
-    140.5662106
+    "Tokyo Tower, Tokyo, Japan",
+    35.6584491,
+    139.74553595504963
   ],
   [
     "Kinkakuji, Kyoto, Japan",
@@ -246,13 +801,23 @@ var addressPoints = [
   ],
   [
     "Tenryuji, Kyoto, Japan",
-    35.0159406,
-    135.6765504
+    35.0153148,
+    135.6772438
   ],
   [
-    "Jojakkoji, Kyoto, Japan",
+    "Jojakko-ji, Kyoto, Japan",
     35.019691699999996,
     135.66864633784195
+  ],
+  [
+    "Nison-in Temple, Kyoto, Japan",
+    35.0215531,
+    135.66783906361843
+  ],
+  [
+    "Giouji, Ukyo Ward, Kyoto, Japan",
+    35.023516900000004,
+    135.66680060601067
   ],
   [
     "Saga Toriimoto Preserved Street, Kyoto, Japan",
@@ -261,8 +826,8 @@ var addressPoints = [
   ],
   [
     "Pontocho Alley, Kyoto, Japan",
-    35.0080674,
-    135.7710399
+    35.00821655,
+    135.77103970000002
   ],
   [
     "Ginkakuji, Kyoto, Japan",
@@ -271,33 +836,38 @@ var addressPoints = [
   ],
   [
     "Nanzenji, Kyoto, Japan",
-    35.011287,
-    135.79024
+    35.0100363,
+    135.7941652
   ],
   [
     "Heian Shrine, Kyoto, Japan",
-    35.015906799999996,
-    135.78289780423577
+    35.0160683,
+    135.78289611102525
   ],
   [
     "Yasaka Shrine, Kyoto, Japan",
     35.00360265,
-    135.77826053837538
+    135.77826108584037
   ],
   [
     "Kodaiji, Kyoto, Japan",
-    35.00174645,
-    135.77985591939222
+    35.0007312,
+    135.7808155
   ],
   [
     "Kiyomizudera, Kyoto, Japan",
-    34.994303,
-    135.7844388864188
+    34.9931123,
+    135.78534
+  ],
+  [
+    "Sannen-zaka, Kyoto, Japan",
+    34.9963405924431,
+    135.78087825681732
   ],
   [
     "Sanmon, Kyoto, Japan",
-    35.1147593,
-    135.7727746
+    35.114759649999996,
+    135.77277195
   ],
   [
     "Fushimi Inari Taisha, Kyoto, Japan",
@@ -306,13 +876,18 @@ var addressPoints = [
   ],
   [
     "Byodoin, Uji, Kyoto, Japan",
-    34.8895001,
-    135.8074525242721
+    34.89020755,
+    135.8071107
   ],
   [
     "Nijojo, Kyoto, Japan",
-    35.0118669,
-    135.7504794
+    35.0140076,
+    135.74853690765354
+  ],
+  [
+    "K\u014dfuku-ji, Nara, Japan",
+    34.6829744,
+    135.8318652600853
   ],
   [
     "Nara National Museum, Nara, Japan",
@@ -325,14 +900,19 @@ var addressPoints = [
     135.83987367510116
   ],
   [
-    "Todaiji, Nara, Japan",
-    34.71272695,
-    135.78870005471012
+    "T\u014ddai-ji Temple, Nara, Japan",
+    34.6874989,
+    135.839814
   ],
   [
-    "Kasugataisha, Nara, Japan",
-    34.6831052,
-    135.8399756
+    "Nigatsu-d\u014d, Nara, Japan",
+    34.689274499999996,
+    135.84426745000002
+  ],
+  [
+    "Kasuga-taisha Shrine, Nara, Japan",
+    34.676810200000006,
+    135.84280093642727
   ],
   [
     "Kongobuji, Koyasan, Wakayama, Japan",
@@ -340,7 +920,12 @@ var addressPoints = [
     135.5838267
   ],
   [
-    "Daimon Gate, Koyasan, Wakayama, Japan",
+    "Konpon Daito, Koyasan, Wakayama, Japan",
+    34.213437049999996,
+    135.5799621
+  ],
+  [
+    "Daimon, Koyasan, Wakayama, Japan",
     34.21314955,
     135.57285804799386
   ],
@@ -351,33 +936,13 @@ var addressPoints = [
   ],
   [
     "Okunoin, Koyasan, Wakayama, Japan",
-    34.2193436,
-    135.60512464045058
-  ],
-  [
-    "Sannenzaka, Kyoto, Japan",
-    34.9974947,
-    135.7810632
-  ],
-  [
-    "Konpon Daito, Koyasan, Wakayama, Japan",
-    34.213437049999996,
-    135.5799621
-  ],
-  [
-    "Giouji, Ukyo Ward, Kyoto, Japan",
-    35.023516900000004,
-    135.66680060601067
-  ],
-  [
-    "K\u014dfuku-ji, Nara, Japan",
-    34.683068399999996,
-    135.8335150936077
+    34.21944655,
+    135.60526078422492
   ],
   [
     "Osaka Castle, Osaka, Japan",
-    34.6873398,
-    135.5258386
+    34.6873735,
+    135.52585552056684
   ],
   [
     "Abeno Harukas, Osaka, Japan",
@@ -401,13 +966,8 @@ var addressPoints = [
   ],
   [
     "Odaiba, Tokyo, Japan",
-    35.619050200000004,
-    139.77936404588297
-  ],
-  [
-    "Tokyo National Museum, Tokyo, Japan",
-    35.71910615,
-    139.77601900459837
+    35.6275347,
+    139.7714719
   ],
   [
     "Tokyo Skytree, Tokyo, Japan",
@@ -415,13 +975,28 @@ var addressPoints = [
     139.81071409992649
   ],
   [
-    "Kasuga-taisha Shrine, Nara, Japan",
-    34.6807615,
-    135.8463146
+    "Tokyo National Museum, Tokyo, Japan",
+    35.719044800000006,
+    139.77596758202066
   ],
   [
-    "T\u014ddai-ji Temple, Nara, Japan",
-    34.6874989,
-    135.839814
+    "Tullahoma, TN, USA",
+    35.3620235,
+    -86.2094342
+  ],
+  [
+    "Knoxville, TN, USA",
+    35.9603948,
+    -83.9210261
+  ],
+  [
+    "Tokai-mura, Naka-gun, Ibaraki-ken, Japan",
+    36.4726607,
+    140.5662106
+  ],
+  [
+    "Bergen, Norway",
+    60.3943055,
+    5.3259192
   ]
 ];
